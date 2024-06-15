a1935d544abb964d393430ca813b3f061ed881b0
Oskar Karpiński <me@medzik.dev>
2024-06-15 22:00:32 +0200

    Disallow to back in navigation stack when changed account

--- ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Home.kt
+++ ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Home.kt
@@ -22,13 +22,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -200,11 +203,17 @@ fun SelectAccount(
         title = stringResource(R.string.SelectAccount),
         items = students,
         onSelected = {
-            navController.navigate(Home(
-                userId = it.id.toString(),
-                platform = it.platform,
-                credentials = it.data
-            ))
+            navController.navigate(
+                Home(
+                    userId = it.id.toString(),
+                    platform = it.platform,
+                    credentials = it.data
+                )
+            ) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    inclusive = true
+                }
+            }
         },
         content = {
             Row(
