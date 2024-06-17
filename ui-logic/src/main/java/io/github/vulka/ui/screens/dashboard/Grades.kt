a0cd662d9bead6374288045a22572b439e964435
rafi612 <rafi612p@outlook.com>
2024-06-17 19:23:47 +0200

    Add grade tabs

--- ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Grades.kt
+++ ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Grades.kt
@@ -1,13 +1,92 @@
 package io.github.vulka.ui.screens.dashboard
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.medzik.android.utils.runOnUiThread
+import io.github.vulka.ui.R
 import kotlinx.serialization.Serializable
+
 
 @Serializable
 object Grades
 
 @Composable
 fun GradesScreen() {
-    Text(text = "Grades")
+    val pagerState = rememberPagerState { 2 }
+    val tabs = listOf(
+        stringResource(R.string.Grades),
+        stringResource(R.string.Summary),
+    )
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = @Composable { tabPositions ->
+                if (pagerState.currentPage < tabPositions.size) {
+                    TabRowDefaults.PrimaryIndicator(
+                        width = 80.dp,
+                        modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        runOnUiThread {
+                            pagerState.scrollToPage(index)
+                        }
+                    },
+                    text = { Text(title) }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { page ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                item {
+                    when (page) {
+                        0 -> GradesTab()
+                        1 -> SummaryTab()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GradesTab() {
+    Text(stringResource(R.string.Grades))
+}
+
+@Composable
+fun SummaryTab() {
+    Text(stringResource(R.string.Summary))
 }
