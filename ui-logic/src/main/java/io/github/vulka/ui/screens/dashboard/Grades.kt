d175078d711ea4570df1c97fb179a7d0fbd32703
Oskar Karpiński <me@medzik.dev>
2024-06-17 22:54:15 +0200

    Add expanding animation in grade card

--- ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Grades.kt
+++ ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/Grades.kt
@@ -1,27 +1,18 @@
 package io.github.vulka.ui.screens.dashboard
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
@@ -34,12 +25,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.medzik.android.components.rememberMutableBoolean
+import dev.medzik.android.components.ui.ExpandedIfNotEmpty
+import dev.medzik.android.components.ui.IconBox
 import dev.medzik.android.utils.runOnUiThread
 import io.github.vulka.core.api.Platform
 import io.github.vulka.core.api.types.Grade
@@ -47,10 +39,8 @@ import io.github.vulka.ui.R
 import io.github.vulka.ui.VulkaViewModel
 import io.github.vulka.ui.common.Avatar
 import io.github.vulka.ui.common.AvatarShape
-import io.github.vulka.ui.utils.getInitials
 import kotlinx.serialization.Serializable
 import java.util.UUID
-
 
 @Serializable
 class Grades(
@@ -173,7 +163,6 @@ fun SubjectCard(
     more: @Composable () -> Unit = {},
     content: @Composable () -> Unit
 ) {
-
     var showMore by rememberMutableBoolean()
 
     Surface(
@@ -198,14 +187,14 @@ fun SubjectCard(
                         .weight(1f)
                 ) {
                     content()
-
                 }
 
-                Icon(if (showMore) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown, contentDescription = null)
-
+                IconBox(
+                    if (showMore) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown
+                )
             }
 
-            if (showMore) {
+            ExpandedIfNotEmpty(Unit.takeIf { showMore }) {
                 Column(
                     modifier = Modifier.padding(8.dp),
                 ) {
@@ -213,10 +202,6 @@ fun SubjectCard(
                 }
             }
         }
-
-
-
-
     }
 }
 
